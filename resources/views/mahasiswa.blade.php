98817ea37722baff76d2ba270fcd526df2ec05db
amircjr01 <amircjr01@gmail.com>
2024-09-19 23:12:01 +0700

    templating bootstrap admin

--- resources/views/mahasiswa.blade.php
+++ resources/views/mahasiswa.blade.php
@@ -1,25 +1,17 @@
-<!DOCTYPE html>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Mahasiswa</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>CRUD</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-<body>
-    @foreach ($mahasiswa as $item)
-        {{ $item->nim }} <br>
-        {{ $item->nama }}
-    @endforeach
-    <div class="container">
-        <h1>Data Mahasiswa</h1>
-        <a href="{{ route('tambah-mahasiswa') }}" class="btn btn-primary">Tambah Data</a>
-        <table class="table table-hover">
+@extends('layouts')
+@section('title')
+Data Mahasiswa
+@endsection
+@section('heading')
+Data Mahasiswa
+@endsection
+@section('content')
+<div class="card-header py-3">
+    <a href="{{ route('tambah-mahasiswa') }}" class="btn btn-primary">Tambah Data</a>
+</div>
+<div class="card-body">
+    <div class="table-responsive">
+        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
             <thead>
                 <tr>
                     <th>#</th>
@@ -29,12 +21,11 @@
                 </tr>
             </thead>
             <tbody>
-@@ -29,6 +30,14 @@
                 @php
                     $i = 1;
                 @endphp
                 @foreach ($mahasiswa as $item)
-                <tr>
+                    <tr>
                         <td>{{ $i++ }}</td>
                         <td>{{ $item->nim }}</td>
                         <td>{{ $item->nama }}</td>
@@ -51,6 +42,5 @@
             </tbody>
         </table>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
-</html>
+</div>
+@endsection
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,18 +1,47 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Profile</title>
-</head>
-<body>
-    <h1>Data Profil</h1>
-    <ul>
-        @foreach ($profile as $item)
-            <li>Nama: {{ $item->nama }}</li>
-            <li>Email: {{ $item->email }}</li>
-        @endforeach
-    </ul>
-</body>
-</html>
+@extends('layouts')
+@section('title')
+Data Profile
+@endsection
+@section('heading')
+Data Profile
+@endsection
+@section('content')
+<div class="card-body">
+    <div class="table-responsive">
+        @if ($errors->any('error'))
+            <div class="alert alert-danger">
+                @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </div>
+        @endif
+        @if (session('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
+        <form method="POST" action="{{ route('profile.change-avatar') }}" enctype="multipart/form-data">
+            @csrf
+            <input type="hidden" name="id" value="{{ $profile->id }}">
+            <div class="mb-3">
+                <label for="name" class="form-label">Nama</label>
+                <input type="text" name="name" class="form-control" id="name" value="{{ $profile->name }}" readonly>
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="text" name="email" class="form-control" id="email" value="{{ $profile->email }}" readonly>
+            </div>
+            <div class="mb-3">
+                <label for="avatar" class="form-label">Foto Profil</label>
+                @if ($profile->avatar != null)
+                    <input type="file" name="avatar" class="form-control" id="avatar">
+                    <img src="{{ asset('storage/uploads/' . $profile->avatar) }}" alt="" style="display: block; margin-top: 12px;">
+                @else
+                    <input type="file" name="avatar" class="form-control" id="avatar">                 
+                @endif
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</div>
+@endsection
